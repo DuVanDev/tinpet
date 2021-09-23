b768898325fe4ac376d0acff414156f8db024ea3
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-09-23 09:39:25 -0500

    Add Icons to Liked and disliked

--- src/components/molecules/PetCard/PetCard.js
+++ src/components/molecules/PetCard/PetCard.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {MdFavorite, MdClose} from 'react-icons/md'
 
 import {Card} from './PetCardStyles'
 
 const PetCard = ({src, isLiked, className}) => {
+  const Icon = isLiked ? (
+    <MdFavorite size={40} color={'#1972D2'} />
+  ) : isLiked == false ? (
+    <MdClose size={40} color={'#fd297B'} />
+  ) : null
+
   return (
-    <Card className={className} isLiked={isLiked}>
+    <Card className={className} $isLiked={isLiked}>
+      {Icon}
       <img draggable={false} src={src}></img>
     </Card>
   )

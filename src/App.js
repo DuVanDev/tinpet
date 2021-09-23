5fda6742dce009528e5e5e80a5341ece10f7a5f1
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-09-23 10:27:56 -0500

    React Helment
    * install React Helment
    * Add Title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import Header from './components/organims/Header/Header'
 import {GlobalStyles} from './Styles/globalStyles'
 
 import Routes from './Routes'
+import {Helmet} from 'react-helmet'
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Helmet>
+        <title> TinPet 🐶</title>
+        <meta name="description" content="Your best app to show photo pets " />
+      </Helmet>
       <div className="App">
         <Header />
         <Routes />

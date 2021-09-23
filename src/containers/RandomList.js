bd9d2360b2b76b0b2ea1860604e43f8be480bf46
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-09-23 01:24:18 -0500

    Detect cuantitive Photo

--- src/containers/RandomList.js
+++ src/containers/RandomList.js
@@ -1,24 +1,32 @@
-import React, {useState} from 'react'
-import { useEffect } from 'react'
-import { GET } from '../utilities/Rest'
+import React, {useCallback, useState} from 'react'
+import {useEffect} from 'react'
+import {GET} from '../utilities/Rest'
 
 const RandomList = ({children}) => {
-  const [randomList, setRandomList] = useState()
+  const [randomList, setRandomList] = useState([])
+  const [index, setIndex] = useState(0)
 
-const getRandomList = async () => {
+  const getRandomList = useCallback(async () => {
     try {
       const list = await GET('https://dog.ceo/api/breeds/image/random/10')
-      setRandomList(list.message)
+      setRandomList([...randomList, ...list.message])
     } catch (error) {
       return console.error(error)
     }
-  }
+  }, [randomList])
+
+  useEffect(() => {
+    if ( randomList.length == 0 ) return
+    if (index > randomList.length - 5) {
+      getRandomList()
+    }
+  }, [index, randomList, getRandomList])
 
   useEffect(() => {
     getRandomList()
   }, [])
 
-  return children({randomList})
+  return children({randomList, index, setIndex})
 }
 
 export default RandomList
